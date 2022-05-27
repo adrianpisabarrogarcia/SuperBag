d5cb9f437ac4c6bf2e69de7dd087b48cb3dee8e0
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2022-05-27 21:01:34 +0200

    Recicle View with a Card anf Firebase DB products list

--- app/src/main/java/com/adriantxupisi/superbag/ListActivity.kt
+++ app/src/main/java/com/adriantxupisi/superbag/ListActivity.kt
@@ -1,46 +1,96 @@
 package com.adriantxupisi.superbag
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.icu.text.SimpleDateFormat
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+import kotlin.collections.ArrayList
 
 enum class ProviderType {
     BASIC
 }
 
 class ListActivity : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var products: ArrayList<Product>
+    private lateinit var productsAdapter: ProductsAdapter
+    private lateinit var db: FirebaseFirestore
+
+    private lateinit var userEmail: String
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         //Setup
         val bundle = intent.extras
-        val email = bundle?.getString("email")
+        userEmail = bundle?.getString("email").toString()
         val provider = bundle?.getString("provider")
-        setup(email ?: "", provider ?: "")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setup(userEmail ?: "", provider ?: "")
+        }
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setup(email: String, provider: String) {
         //Toolbar initialization
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-
         //RecyclerView initialization
-        val recyclerView = findViewById<RecyclerView>(R.id.listProductsRecyclerView)
+        recyclerView = findViewById<RecyclerView>(R.id.listProductsRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
         //List of Products
-        val products = ArrayList<Product>()
-        recyclerView.adapter = ProductsAdapter(products)
+        products = arrayListOf()
+
+        //Adapter of Productos
+        productsAdapter = ProductsAdapter(products)
+
+        //Add products to the list
+        recyclerView.adapter = productsAdapter
+
+
+        EventChangeListener()
 
 
         //Print information of the user on the console
         println("Email: $email")
         println("Provider: $provider")
+    }
+
+    //Add products to the list
+    private fun EventChangeListener() {
+        //Listener for the products
+        db = FirebaseFirestore.getInstance()
+        db.collection("products").addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            if (firebaseFirestoreException != null) {
+                println("Error: ${firebaseFirestoreException.message}")
+            } else {
+                products.clear()
+                for (document in querySnapshot!!) {
+                    val product = document.toObject(Product::class.java)
+                    products.add(product)
+                }
+                productsAdapter.notifyDataSetChanged()
+            }
+        }
     }
 
     //Toolbar menu
@@ -71,7 +121,10 @@ class ListActivity : AppCompatActivity() {
 
     //Go to new item activity to register a new item
     private fun goNewItem() {
-        val newItemActivityIntent = Intent(this, NewItemActivity::class.java)
+        val newItemActivityIntent = Intent(this, NewItemActivity::class.java).apply {
+            //Send the userEmail to the new item activity
+            putExtra("userEmail", userEmail)
+        }
         startActivity(newItemActivityIntent)
     }
 
--- app/src/main/java/com/adriantxupisi/superbag/ProductsAdapter.kt
+++ app/src/main/java/com/adriantxupisi/superbag/ProductsAdapter.kt
@@ -1,27 +1,38 @@
 package com.adriantxupisi.superbag
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ProductsAdapter(val products: List<Product>) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+class ProductsAdapter(private val products: ArrayList<Product>) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
 
     //Cuando el recyclerView necesita una vista para mostrar un elemento,
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_products , parent, false)
+        return ViewHolder(view)
     }
 
     //Cuando actualizamos el recyclerView, se llama a este método
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val product = products[position]
+        holder.bind(product)
     }
 
     //Devuelve el número de elementos que contiene el recyclerView
-    override fun getItemCount(): Int {
-        return products.size
-    }
+    override fun getItemCount(): Int = products.size
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        private val name = itemView.findViewById<TextView>(R.id.productNameTextView)
+        private val quantity = itemView.findViewById<TextView>(R.id.productQuantityTextView)
+        private val description = itemView.findViewById<TextView>(R.id.productDescriptionTextView)
+
+        fun bind(product: Product) {
+            name.text = product.name.toString()
+            quantity.text = product.quantity.toString()
+            description.text = product.description.toString()
+        }
 
     }
 
